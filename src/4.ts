c0a06a05b38301b9c41aab3596647aec8f2d9483
Pavel Pereverziev <pereverzev.pavel1975@gmail.com>
2023-12-21 11:56:49 +0200

    finished

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,62 @@
+interface IKey {
+  getSignature(): number;
+}
+class Key implements IKey {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+interface IPerson {
+  getKey(): IKey;
+}
+
+class Person implements IPerson {
+  private key: IKey;
+  constructor(key: IKey) {
+    this.key = key;
+  }
+  getKey(): IKey {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: IKey;
+  protected tenants: IPerson[] = [];
+
+  constructor(key: IKey) {
+    this.key = key;
+  }
+
+  comeIn(person: IPerson): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log('Person came in.');
+    } else {
+      console.log('The door is closed.');
+    }
+  }
+
+  abstract openDoor(key: IKey): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: IKey): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log('The door is open.');
+    } else {
+      console.log('The door steal closed.');
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +65,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
